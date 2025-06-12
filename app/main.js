fad9727856491b626920f8353156d7605d77c537
luanarruda <luanamarrudaa@gmail.com>
2025-06-12 12:31:02 -0300

    refactor: split methods into separate JavaScript files

--- app/main.js
+++ app/main.js
@@ -4,8 +4,6 @@ const endpointDaApi = 'https://guilhermeonrails.github.io/casadocodigo/livros.js
 // funcao de busca dos livros
 getBuscarLivrosDaApi()
 
-const elementoParaInserirLivros = document.getElementById('livros');
-
 async function getBuscarLivrosDaApi() {
 	const res = await fetch(endpointDaApi);
 	livros = await res.json()
@@ -13,19 +11,3 @@ async function getBuscarLivrosDaApi() {
 	exibirLivrosNaTela(livros);
 }
 
-//função para exibir os livros na tela
-// e inserir no elemento HTML
-function exibirLivrosNaTela(listaDeLivros) {
-	listaDeLivros.forEach(livro => {
-		elementoParaInserirLivros.innerHTML += `
-		<div class="livro">
-			<img class="livro__imagem" src="${livro.imagem}" alt="${livro.alt}">
-			<h2 class="livro__titulo">${livro.titulo}</h2>
-			<p class="livro__descricao">${livro.autor}</p>
-			<p class="livro__preco">R$ ${livro.preco.toFixed(2)}</p>
-			<div class="tags">
-        <span class="tag">${livro.categoria}</span>
-      </div>`
-
-	});
-}
---
+++ app/metodoForEach.js
@@ -0,0 +1,19 @@
+const elementoParaInserirLivros = document.getElementById('livros');
+
+
+//função para exibir os livros na tela
+// e inserir no elemento HTML
+function exibirLivrosNaTela(listaDeLivros) {
+	listaDeLivros.forEach(livro => {
+		elementoParaInserirLivros.innerHTML += `
+		<div class="livro">
+			<img class="livro__imagem" src="${livro.imagem}" alt="${livro.alt}">
+			<h2 class="livro__titulo">${livro.titulo}</h2>
+			<p class="livro__descricao">${livro.autor}</p>
+			<p class="livro__preco">R$ ${livro.preco.toFixed(2)}</p>
+			<div class="tags">
+        <span class="tag">${livro.categoria}</span>
+      </div>`
+
+	});
+}
